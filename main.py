a8e7e66860bbbf9ba3a48ec56b9d9adadec37868
xyrhofire <49827090+xyrhofire@users.noreply.github.com>
2023-10-22 15:57:01 +0000

    Testing

--- main.py
+++ main.py
@@ -4,24 +4,27 @@
 # Program give hint if the number too high or low
 # Keep track of tries
 
-
 import random
 
+
 def pick_number(start, end):
-    return random.randint(start, end)
+  return random.randint(start, end)
+
 
 number = pick_number(1, 100)
-print(number)  # This will show the number, if you want it to be a surprise, remove this line
+print(
+    number
+)  # This will show the number, if you want it to be a surprise, remove this line
 
 while True:
   guess = int(input("Guess a number from 1 to 100: "))
-    
+
   if guess == number:
     print("Correct")
     break
   elif guess > number:
     print("Incorrect! The number is smaller, please guess again: ")
-      
+
   elif guess < number:
     print("Incorrect! The number is bigger, please guess again: ")
 
